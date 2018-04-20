1c1c35896b2d371d51690c47737fdc9e69323fa2
K <x00.x7f@gmail.com>
2018-04-20 19:24:36 +0900

    first commit

--- touchbar-emoji/AppDelegate.m
+++ touchbar-emoji/AppDelegate.m
@@ -7,16 +7,51 @@
 //
 
 #import "AppDelegate.h"
+#import "TouchBar.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () <NSTouchBarDelegate>
 
 @property (weak) IBOutlet NSWindow *window;
 @end
 
+NSTouchBar *_groupTouchBar;
+
 @implementation AppDelegate
 
+static const NSTouchBarItemIdentifier kStripButton = @"codehex.stripbtn";
+
+- (NSTouchBar *)groupTouchBar {
+    if (!_groupTouchBar) {
+        NSTouchBar *groupTouchBar = [[NSTouchBar alloc] init];
+        //groupTouchBar.defaultItemIdentifiers = @[ kButton1, kButton2 ];
+        groupTouchBar.delegate = self;
+        _groupTouchBar = groupTouchBar;
+    }
+    return _groupTouchBar;
+}
+
+- (void)present:(id)sender {
+    NSLog(@"Hello!!");
+    [NSTouchBar presentSystemModalFunctionBar:self.groupTouchBar systemTrayItemIdentifier:kStripButton];
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    if (@available(macOS 10.12.2, *)) {
+        DFRSystemModalShowsCloseBoxWhenFrontMost(YES);
+        NSCustomTouchBarItem *beer = [[NSCustomTouchBarItem alloc] initWithIdentifier: kStripButton];
+        beer.view = [NSButton buttonWithTitle:@"🍺" target:self action:@selector(present:)];
+        [NSTouchBarItem addSystemTrayItem:beer];
+        DFRElementSetControlStripPresenceForIdentifier(kStripButton, YES);
+    } else {
+        [self applicationDoesNotSupport];
+    }
+}
+
+- (void)applicationDoesNotSupport {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"OK"];
+    [alert setMessageText:@"Sorry"];
+    [alert setInformativeText:@"Does not support your machine or version"];
 }
 
 
--- touchbar-emoji/TouchBar.h
+++ touchbar-emoji/TouchBar.h
@@ -10,4 +10,21 @@
 #define touchbar_h
 
 
+#import <AppKit/AppKit.h>
+
+extern void DFRElementSetControlStripPresenceForIdentifier(NSString *, BOOL);
+extern void DFRSystemModalShowsCloseBoxWhenFrontMost(BOOL);
+
+@interface NSTouchBarItem ()
+
++ (void)addSystemTrayItem:(NSTouchBarItem *)item;
+
+@end
+
+@interface NSTouchBar ()
+
++ (void)presentSystemModalFunctionBar:(NSTouchBar *)touchBar systemTrayItemIdentifier:(NSString *)identifier;
+
+@end
+
 #endif /* touchbar_h */
