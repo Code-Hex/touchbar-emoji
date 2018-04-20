48d7329636cd4ea2f60e39d69cb202b115ae0f03
K <x00.x7f@gmail.com>
2018-04-20 19:24:18 +0900

    Initial commit

---
+++ touchbar-emoji/TouchBar.h
@@ -0,0 +1,13 @@
+//
+//  touchbar.h
+//  touchbar-emoji
+//
+//  Created by codehex on 2018/04/19.
+//  Copyright © 2018年 codehex. All rights reserved.
+//
+
+#ifndef touchbar_h
+#define touchbar_h
+
+
+#endif /* touchbar_h */
